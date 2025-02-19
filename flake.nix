7de7e7b65b7de0da48d7aa6caa8b5d035f517fd3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-19 13:44:21 +0000

    Nix Flake support added, bug found, incompatibility with latest Boost 1.87.0

--- flake.nix
+++ flake.nix
@@ -12,8 +12,12 @@
     nixpkgs,
     utils,
   }:
+    # utils.lib.eachDefaultSystem (system: let
+    #   pkgs = import nixpkgs {inherit system;};
     utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+    pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
+      boost = prev.boost.override { version = "1.87.0"; }; # Specify a recent version
+    });
     in {
       packages.default = pkgs.stdenv.mkDerivation {
         pname = "zosclib_flake";
@@ -27,15 +31,8 @@
         ];
 
         buildInputs = with pkgs; [ # Libraries for development shell
-          gcc # includes libatomic
+          gcc
           boost
-          # readline
-          # alsa-lib # For audio support
-          # freetype # For font rendering
-          # fontconfig # For font configuration
-          # libGL # OpenGL support
-          # curl # For libcurl
-          # webkitgtk_4_0
         ];
 
         cmakeFlags = [];
@@ -51,19 +48,21 @@
 
       devShells.default = pkgs.mkShell rec {
         buildInputs = with pkgs; [
-          just
           gcc # Use GCC explicitly
           cmake
           pkg-config
           boost
-          # readline
-          # alsa-lib # Audio support
-          # freetype # Font rendering
-          # fontconfig # Font configuration
-          # libGL # OpenGL
-          # curl # libcurl
-          # webkitgtk_4_0
         ];
+
+        # Set environment variables to help the compiler find Boost
+        shellHook = ''
+          export CC=${pkgs.gcc}/bin/gcc
+          export CXX=${pkgs.gcc}/bin/g++
+          export LIBRARY_PATH="${pkgs.boost.out}/lib:${pkgs.gcc.cc}/lib:${pkgs.gcc.cc}/lib64"
+          export NIX_CFLAGS_COMPILE="-I${pkgs.boost.dev}/include -I${pkgs.gcc.cc}/include"
+          export NIX_LDFLAGS="-L${pkgs.boost.out}/lib"
+          echo "Development environment loaded with GCC, Boost, CMake, and pkg-config."
+        '';
       };
     })
 
